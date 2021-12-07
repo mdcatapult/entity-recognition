c1713315d3c2e7621bb12c6e86fe6350161649d0
Nick Etherington <nick.etherington@md.catapult.org.uk>
2021-12-07 08:42:34 +0000

    Replaces custom CORS middleware with CORS gin's middleware.

--- go/cmd/recognition-api/main.go
+++ go/cmd/recognition-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -77,8 +78,17 @@ func main() {
 		}
 	}
 
-	r := gin.New()
-	r.Use(gin.LoggerWithFormatter(lib.JsonLogFormatter), CORSMiddleware())
+	r := gin.Default()
+	r.Use(
+		gin.LoggerWithFormatter(lib.JsonLogFormatter),
+		cors.New(cors.Config{
+			AllowOrigins:     []string{"*"},
+			AllowMethods:     []string{"GET", "POST"},
+			AllowHeaders:     []string{"Origin", "Content-Type"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}),
+	)
 
 	c := controller{
 		recognisers: recogniserClients,
@@ -104,18 +114,4 @@ func loadBlacklist(path string) blacklist.Blacklist {
 		bl = *loadedBlacklist
 	}
 	return bl
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	}
 }
