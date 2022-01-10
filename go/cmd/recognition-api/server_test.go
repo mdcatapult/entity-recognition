3566d1eecb1ffedee5d56936cee01bbfede003b9
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2022-01-10 13:21:41 +0000

    replace gomega with Expect

--- go/cmd/recognition-api/server_test.go
+++ go/cmd/recognition-api/server_test.go
@@ -45,15 +45,15 @@ var _ = Describe("GetRecognisers", func() {
 		var _ = It("Should be a bad request when no recognisers are specified", func() {
 			res, err := http.Get("http://localhost:9999/statusCodeTests")
 
-			Ω(err).Should(BeNil())
-			Ω(res.StatusCode).Should(Equal(http.StatusBadRequest))
+			Expect(err).Should(BeNil())
+			Expect(res.StatusCode).Should(Equal(http.StatusBadRequest))
 		})
 
 		var _ = It("Should return status OK", func() {
 			res, err := http.Get("http://localhost:9999/statusCodeTests?recogniser=something")
 
-			Ω(err).Should(BeNil())
-			Ω(res.StatusCode).Should(Equal(http.StatusOK))
+			Expect(err).Should(BeNil())
+			Expect(res.StatusCode).Should(Equal(http.StatusOK))
 		})
 	})
 
@@ -78,37 +78,37 @@ var _ = Describe("GetRecognisers", func() {
 
 			singleRecogniserAsserter := func(c *gin.Context) {
 				receivedRecogniser, ok := c.Get(recognisersKey)
-				Ω(ok).Should(Equal(true))
+				Expect(ok).Should(Equal(true))
 
 				recognisers, ok := receivedRecogniser.([]lib.RecogniserOptions)
 
-				Ω(ok).Should(Equal(true))
-				Ω(len(recognisers)).Should(Equal(1))
-				Ω(recognisers[0].Name).Should(Equal(recogniser1))
+				Expect(ok).Should(Equal(true))
+				Expect(len(recognisers)).Should(Equal(1))
+				Expect(recognisers[0].Name).Should(Equal(recogniser1))
 			}
 
 			multipleRecognisersAsserter := func(c *gin.Context) {
 
 				receivedRecognisers, ok := c.Get(recognisersKey)
-				Ω(ok).Should(Equal(true))
+				Expect(ok).Should(Equal(true))
 
 				recognisers, ok := receivedRecognisers.([]lib.RecogniserOptions)
 
-				Ω(ok).Should(Equal(true))
-				Ω(len(recognisers)).Should(Equal(2))
-				Ω(recognisers[0].Name).Should(Equal(recogniser1))
-				Ω(recognisers[1].Name).Should(Equal(recogniser2))
+				Expect(ok).Should(Equal(true))
+				Expect(len(recognisers)).Should(Equal(2))
+				Expect(recognisers[0].Name).Should(Equal(recogniser1))
+				Expect(recognisers[1].Name).Should(Equal(recogniser2))
 			}
 
 			allRecognisersAsserter := func(c *gin.Context) {
 				receivedRecogniser, ok := c.Get(recognisersKey)
-				Ω(ok).Should(Equal(true))
+				Expect(ok).Should(Equal(true))
 
 				recognisers, ok := receivedRecogniser.([]lib.RecogniserOptions)
 
-				Ω(ok).Should(Equal(true))
-				Ω(len(recognisers)).Should(Equal(1))
-				Ω(recognisers[0].Name).Should(Equal(recogniser3))
+				Expect(ok).Should(Equal(true))
+				Expect(len(recognisers)).Should(Equal(1))
+				Expect(recognisers[0].Name).Should(Equal(recogniser3))
 			}
 
 			router.GET("/singleRecogniser", server{}.GetRecognisers, singleRecogniserAsserter)
@@ -124,20 +124,20 @@ var _ = Describe("GetRecognisers", func() {
 
 		var _ = It("Should add single recogniser to context", func() {
 			res, err := http.Get(fmt.Sprintf("http://localhost:9998/singleRecogniser?%v=%v", "recogniser", recogniser1))
-			Ω(err).Should(BeNil())
-			Ω(res.StatusCode).Should(Equal(http.StatusOK))
+			Expect(err).Should(BeNil())
+			Expect(res.StatusCode).Should(Equal(http.StatusOK))
 		})
 
 		var _ = It("Should add multiple recognisers to context", func() {
 			res, err := http.Get(fmt.Sprintf("http://localhost:9998/multipleRecognisers?%v=%v&%v=%v", "recogniser", recogniser1, "recogniser", recogniser2))
-			Ω(err).Should(BeNil())
-			Ω(res.StatusCode).Should(Equal(http.StatusOK))
+			Expect(err).Should(BeNil())
+			Expect(res.StatusCode).Should(Equal(http.StatusOK))
 		})
 
 		var _ = It("Should use the allRecognisers flag to use all available recognisers", func() {
 			res, err := http.Get("http://localhost:9998/allRecognisers?allRecognisers=true")
-			Ω(err).Should(BeNil())
-			Ω(res.StatusCode).Should(Equal(http.StatusOK))
+			Expect(err).Should(BeNil())
+			Expect(res.StatusCode).Should(Equal(http.StatusOK))
 		})
 	})
 })
