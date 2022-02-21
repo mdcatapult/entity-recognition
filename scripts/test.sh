6fc5f8ade2e8e48b3d685335e252a3c5eb5bb526
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2022-02-21 13:38:26 +0000

    correct exit code from script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,8 @@ bin/dictionary &
 sleep 2 && # services need time to spin up
 bin/recognition-api & 
 
-
 export NER_API_TEST=yes
 go test -v -run=TestAPI ./...
+result=$?
 unset NER_API_TEST
+exit $result
