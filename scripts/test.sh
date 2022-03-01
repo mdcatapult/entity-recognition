30d99d97770b55b544e59557f414be47f5753733
Alex Whitehorn <alex.whitehorn@md.catapult.org.uk>
2022-03-01 14:37:22 +0000

    fix CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ testSuite=$1
 dict=$2
 format=$3
 
-bin/dictionary-importer dictionaryPath=$dict dictionaryFormat=$format &
+bin/dictionary-importer dictionaryPath=$dict dictionaryFormat=$format &&
 bin/regexer & 
 bin/dictionary &
 sleep 2 && # services need time to spin up
@@ -13,4 +13,5 @@ export NER_API_TEST=yes
 go test -v -run=$1 ./...
 result=$?
 unset NER_API_TEST
+pkill -P $$
 exit $result
